6be624cdb9eda64cad378039464f314e04e400b7
jkxruby <jkxruby@gmail.com>
2017-06-27 16:52:01 +0800

    补充8-3额外作业忘记的update

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,5 @@
 class PostsController < ApplicationController
-  before_action :authenticate_user!, :only => [:new, :create]
+  before_action :authenticate_user!, :only => [:new, :create, :edit, :destroy, :update]
 
 def new
   @group = Group.find(params[:group_id])
@@ -31,6 +31,18 @@ def destroy
   @post.destroy
   redirect_to account_posts_path, notice: "删除快乐！"
 end
+
+def update
+  @group = Group.find(params[:group_id])
+  @post = Post.find(params[:id])
+  @post.group = @group
+   if @post.update(post_params)
+     redirect_to account_posts_path, notice: "更新真好！"
+   else
+     render :edit
+   end
+ end
+
 
 private
 def post_params
