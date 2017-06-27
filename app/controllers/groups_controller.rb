4d368271850cab9988f8f9efdc0bdc2ffee5cb41
jkxruby <jkxruby@gmail.com>
2017-06-27 13:54:25 +0800

    when created a group, join it automatically

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -21,6 +21,7 @@ class GroupsController < ApplicationController
     @group = Group.new(group_params)
     @group.user = current_user
     if @group.save
+      current_user.join!(@group)
     redirect_to groups_path
   else
     render :new
@@ -76,6 +77,6 @@ end
       if current_user != @group.user
         redirect_to root_path, alert: "you have no permission"
       end
-end 
+end
 
 end
